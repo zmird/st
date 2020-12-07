bc8919722e9f40bb8807f72dc668ca0369b0dc6e
Zakaria MIRD <z.mird@cineca.it>
2020-12-07 03:25:15 +0100

    Modified keys

--- config.def.h
+++ config.def.h
@@ -188,8 +188,8 @@ static uint forcemousemod = ShiftMask;
 const unsigned int mousescrollincrement = 2;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = mousescrollincrement},      0, /* !alt */ -1 },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = mousescrollincrement},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,            Button4, kscrollup,      {.i = mousescrollincrement},      0, /* !alt */ -1 },
+	{ XK_ANY_MOD,            Button5, kscrolldown,    {.i = mousescrollincrement},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
